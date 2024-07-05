64c47d557e1c2ebb71269c11ac8776174d87c470
SofiiaPol <sophia.polyakova05@gmail.com>
2024-07-05 20:14:54 +0300

    finish task

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,14 @@ import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
 
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+  const getInitialContacts = () => {
+    const storedContacts = localStorage.getItem("contacts");
+    return storedContacts ? JSON.parse(storedContacts) : [];
+  };
+
+  const [contacts, setContacts] = useState(getInitialContacts());
 
   const [filter, setFilter] = useState("");
-
-  useEffect(() => {
-    const storedContacts = localStorage.getItem("contacts");
-    if (storedContacts) {
-      setContacts(JSON.parse(storedContacts));
-    }
-  }, []);
 
   useEffect(() => {
     localStorage.setItem("contacts", JSON.stringify(contacts));
@@ -52,8 +45,3 @@ function App() {
 }
 
 export default App;
-
-// { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-//     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-//     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-//     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
